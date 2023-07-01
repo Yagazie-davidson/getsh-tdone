0b3c16d658a25c411fc2eca1fcffa0db820c2655
letam <davidsonletam@gmail.com>
2023-07-01 23:30:36 +0100

    fetch service

---
+++ client/src/service/fetch.js
@@ -0,0 +1,12 @@
+export const fetchRequest = async (url, method, payload) => {
+  const res = await fetch(`http://localhost:9000/${url}`, {
+    method: method, // HTTP method (GET, POST, PUT, DELETE, etc.)
+    headers: {
+      "Content-Type": "application/json", // Set the content type of the request body
+      // Add any additional headers if required
+    },
+    body: JSON.stringify(payload), // Convert the data object to a JSON string
+  });
+  const data = await res.json();
+  return data;
+};
