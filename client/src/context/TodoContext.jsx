7df1439d5b90983fd8a20b52935afd65422c2b9c
letam <davidsonletam@gmail.com>
2023-07-04 21:24:59 +0100

    Add loading state feature to the context

--- client/src/context/TodoContext.jsx
+++ client/src/context/TodoContext.jsx
@@ -5,14 +5,17 @@ const TodoContext = createContext();
 
 export function TodoProvider({ children }) {
   const [listTask, setListTask] = useState(null);
+  const [loading, setLoading] = useState(false);
   // Get all todo
   const getTask = async () => {
+    setLoading(true);
     const data = await fetchRequest("todos", "GET");
     setListTask(data);
     console.log(data);
+    setLoading(false);
   };
   return (
-    <TodoContext.Provider value={{ listTask, getTask }}>
+    <TodoContext.Provider value={{ listTask, getTask, loading, setLoading }}>
       {children}
     </TodoContext.Provider>
   );
