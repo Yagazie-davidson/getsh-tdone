405ef7c5cbf451e0b97396b93f29fb11a514d7e5
letam <davidsonletam@gmail.com>
2023-07-04 21:24:42 +0100

    Implement Loading state

--- client/src/components/GetAllTask.jsx
+++ client/src/components/GetAllTask.jsx
@@ -2,7 +2,7 @@ import { useContext, useEffect } from "react";
 import TodoContext from "../context/TodoContext";
 import DeleteTask from "./DeleteTask";
 function GetAllTask() {
-  const { listTask, getTask } = useContext(TodoContext);
+  const { listTask, getTask, loading } = useContext(TodoContext);
 
   useEffect(() => {
     getTask(); // fetch all task
@@ -11,30 +11,36 @@ function GetAllTask() {
   return (
     <>
       <div>
-        {listTask && //map out the task list
-          listTask.todos.map((item) => {
-            return (
-              <div key={item._id} className="flex items-center ">
-                <input
-                  placeholder="eg: Get eggs from the store"
-                  type="text"
-                  value={item.todo}
-                  disabled
-                  className={`mt-1 px-3 py-2 bg-white block sm:text-sm`}
-                  aria-label="Input box for todos"
-                />
-                <div className=" flex  items-center space-x-2">
-                  <input
-                    type="checkbox"
-                    onClick={(e) => {
-                      console.log(e.target.value);
-                    }}
-                  />
-                  <DeleteTask id={item._id} />
-                </div>
-              </div>
-            );
-          })}
+        {loading ? (
+          <p className="mb-10">Loading...</p> // Loading state
+        ) : (
+          <>
+            {listTask && //map out the task list
+              listTask.todos.map((item) => {
+                return (
+                  <div key={item._id} className="flex items-center ">
+                    <input
+                      placeholder="eg: Get eggs from the store"
+                      type="text"
+                      value={item.todo}
+                      disabled
+                      className={`mt-1 px-3 py-2 bg-white block sm:text-sm`}
+                      aria-label="Input box for todos"
+                    />
+                    <div className=" flex  items-center space-x-2">
+                      <input
+                        type="checkbox"
+                        onClick={(e) => {
+                          console.log(e.target.value);
+                        }}
+                      />
+                      <DeleteTask id={item._id} />
+                    </div>
+                  </div>
+                );
+              })}
+          </>
+        )}
       </div>
     </>
   );
