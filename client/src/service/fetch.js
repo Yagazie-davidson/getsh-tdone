199b5151e73d8c3639a3227d8c6140515fdcaae2
letam <davidsonletam@gmail.com>
2023-07-04 21:02:57 +0100

    Implement try and catch block

--- client/src/service/fetch.js
+++ client/src/service/fetch.js
@@ -1,24 +1,32 @@
 export const fetchRequest = async (url, method, payload) => {
-  const res = await fetch(`http://localhost:9000/${url}`, {
-    method: method, // HTTP method (GET, POST, PUT, DELETE, etc.)
-    headers: {
-      "Content-Type": "application/json", // Set the content type of the request body
-      // Add any additional headers if required
-    },
-    body: JSON.stringify(payload), // Convert the data object to a JSON string
-  });
-  const data = await res.json();
-  return data;
+  try {
+    const res = await fetch(`http://localhost:9000/${url}`, {
+      method: method, // HTTP method (GET, POST, PUT, DELETE, etc.)
+      headers: {
+        "Content-Type": "application/json", // Set the content type of the request body
+        // Add any additional headers if required
+      },
+      body: JSON.stringify(payload), // Convert the data object to a JSON string
+    });
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export const getTodos = async () => {
-  const res = await fetch(`http://localhost:9000/todos`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json", // Set the content type of the request body
-      // Add any additional headers if required
-    },
-  });
-  const data = await res.json();
-  return data;
+  try {
+    const res = await fetch(`http://localhost:9000/todos`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json", // Set the content type of the request body
+        // Add any additional headers if required
+      },
+    });
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
 };
